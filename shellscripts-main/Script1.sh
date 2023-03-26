d16eecf47886f0c5110ecf49dcc6f1ad4b13134a
tushee <abc@example.com>
2023-03-26 07:16:05 +0530

    first commit

---
+++ shellscripts-main/Script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
+echo "Best class of AWS/DevOps"
+echo "We are learning git"
+echo "We are done with git"
+echo "This example is of branching strategy"
+echo "Hello Batch-16, We are learning Git/Github"
+echo "Good Evening"
+echo "We are changing script1"
+echo "We have applied this changes in main branch"
+
